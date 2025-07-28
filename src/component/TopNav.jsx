0c50c65cf7872144d52bae3ad59e98d0b024bd19
Ayomide-design <ademirinsola414@gmail.com>
2025-07-28 02:32:00 +0100

    styling nav-bar

--- src/component/TopNav.jsx
+++ src/component/TopNav.jsx
@@ -1,33 +1,36 @@
 import React from "react";
 import { RxDashboard } from "react-icons/rx";
 import { HiOutlineBell } from "react-icons/hi2";
+import { CiSearch } from "react-icons/ci";
 
 function TopNav() {
     return (
-        <div className="w-full flex justify-betweeen px-4 py-2">
+        <div className="w-full flex justify-betweeen ">
 
-            <div className="flex items-center gap-3">
-                <RxDashboard className="text-2xl text-gray-400" />
-                <button className="text-sm font-medium text-gray-700 border"> 
+            <div className="flex items-center gap-3 py-3">
+                <RxDashboard className="text-3xl text-gray-400" />
+                <button className="text-sm font-medium text-gray-700"> 
                     User Management
                 </button>
             </div>
-            <div className="flex justify-between ml-120">
-                <div className="text-gray-400 text-sm  ">
+            {/* <CiSearch /> */}
+            <div className=" ml-80 px-4 py-4">
+                <div className=" flex items-center text-gray-400 text- sm font-normal border px-15 py-2 rounded-full "> 
                     Search for anything
+                    <CiSearch size={10}/>
                 </div>
             </div>
-            <div className="flex gap-4">
+            <div className="flex items-center gap-4 mb-10 py-4">
                 <button className="text-gray-400 hover:text-blue-600">
-                    <HiOutlineBell className="text-2xl" />
+                    <HiOutlineBell/>
                 </button>
-                <button className="bg-blue-600 text-gray-400 rounded-full px-3 py-1 text-sm font-semibold">
-                    SA
+                <div className="flex items-center border rounded-full px-2">
+                <button className="text-gray-400 rounded-full px-3 py-1 text-sm font-semibold">
+                    SA 
                 </button>
-
-
-
                 <img src="/logo1.svg" alt="Logo" className="h-8 w-auto" />
+                </div>
+
             </div>
 
         </div>
