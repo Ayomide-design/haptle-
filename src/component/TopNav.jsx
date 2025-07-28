0d6416ce9759ad6d96df6da50a7cf697cd4c6aa9
Ayomide-design <ademirinsola414@gmail.com>
2025-07-28 22:52:51 +0100

    adjusted the nav-bar

--- src/component/TopNav.jsx
+++ src/component/TopNav.jsx
@@ -5,7 +5,7 @@ import { CiSearch } from "react-icons/ci";
 
 function TopNav() {
     return (
-        <div className="w-full flex justify-betweeen ">
+        <div className="w-full flex justify-betweeen items-center ">
 
             <div className="flex items-center gap-3 py-3 ml-8">
                 <RxDashboard className="text-3xl text-gray-400" />
@@ -17,17 +17,21 @@ function TopNav() {
             <div className=" ml-80 px-4 py-4">
                 <div className=" flex items-center text-gray-400 text- sm font-normal border px-15 py-2 rounded-full "> 
                     Search for anything
-                    <CiSearch size={10}/>
                 </div>
+                {/* relative */}
+                    {/* absolute */}
+                    <div className="relative h-[100%]">
+                        <CiSearch size={20} className="absolute bottom-3 left-5"/>
+                    </div>
             </div>
-            <div className="flex items-center gap-4 mb-10 py-4">
+            <div className="flex items-center gap-4 py-4 h-[100%]">
                 <button className="text-gray-400 ">
                     <HiOutlineBell size={20}/>
                 </button>
-                <div className="flex items-center border rounded-full px-2">
-                <button className="text-[#5856D6] px-3 py-1 text-sm font-semibold border rounded-full ">
-                    SA 
-                </button>
+                <div className="flex items-center border rounded-full p-2">
+                    <div className="text-[#5856D6] px-3 py-1 text-sm font-semibold border rounded-full ">
+                        SA 
+                    </div>
                 <img src="/logo1.svg" alt="Logo" className="h-8 w-auto" />
                 </div>
 
