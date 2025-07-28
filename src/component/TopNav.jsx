d44c0f37d3885a85e26ccc399cd0c20feb3df0d5
Ayomide-design <ademirinsola414@gmail.com>
2025-07-28 02:56:42 +0100

    styling nav-bar

--- src/component/TopNav.jsx
+++ src/component/TopNav.jsx
@@ -7,7 +7,7 @@ function TopNav() {
     return (
         <div className="w-full flex justify-betweeen ">
 
-            <div className="flex items-center gap-3 py-3">
+            <div className="flex items-center gap-3 py-3 ml-8">
                 <RxDashboard className="text-3xl text-gray-400" />
                 <button className="text-sm font-medium text-gray-700"> 
                     User Management
@@ -21,11 +21,11 @@ function TopNav() {
                 </div>
             </div>
             <div className="flex items-center gap-4 mb-10 py-4">
-                <button className="text-gray-400 hover:text-blue-600">
-                    <HiOutlineBell/>
+                <button className="text-gray-400 ">
+                    <HiOutlineBell size={20}/>
                 </button>
                 <div className="flex items-center border rounded-full px-2">
-                <button className="text-gray-400 rounded-full px-3 py-1 text-sm font-semibold">
+                <button className="text-[#5856D6] px-3 py-1 text-sm font-semibold border rounded-full ">
                     SA 
                 </button>
                 <img src="/logo1.svg" alt="Logo" className="h-8 w-auto" />
